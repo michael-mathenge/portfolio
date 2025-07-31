6ca809462c98308b8d677f73f1e018be7d51855e
michael-mathenge <mmathenge1@gmail.com>
2025-07-31 19:23:47 +0300

    Fix deployment issue by making debug_toolbar import and URL inclusion conditional

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,15 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
-
-from portfolio import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('datascience/', include('datascience.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
